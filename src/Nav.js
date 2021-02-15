1cb29cf64dfa67e34f71fd1040f1d4149fb408be
Rahul Singh <rahulsingh10041998@gmail.com>
2021-02-15 17:16:08 +0530

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,39 @@
+import react,{useState,useEffect} from 'react';
+import './Nav.css';
+
+
+const Nav = ()=>{
+
+    const [show,handleShow] = useState(false);
+
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY>100){
+                handleShow(true);
+            }else{
+                handleShow(false);
+            }
+
+        })
+        return ()=>{
+            window.removeEventListener("scroll");
+        }
+    },[]);
+    
+    return(
+        <div className={`nav ${show && "nav__black"}`}>
+            <img className="nav__logo"
+             src="https://upload.wikimedia.org/wikipedia/commons/1/15/Logonfx.png"
+             alt="Netflix Logo"
+            />
+
+             <img className="nav__avatar" 
+             src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+             alt="Netflix Avatar"
+             />
+        </div>
+    )
+}
+
+export default Nav;
+
---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "9b2c4d031941c368933bd94b665fc7b0";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=13`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanticMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests;
